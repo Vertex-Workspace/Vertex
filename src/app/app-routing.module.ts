0f197daec2f2f600521e37eaa9c776f4eb5ae96e
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-08-29 19:01:34 -0300

    implementa importação e possibilidade de tailwind css nos arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { MuralComponent } from './pages/mural/mural.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'mural',
+    component: MuralComponent
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "login",
+  },
+  {
+    path: "**",
+    redirectTo: "login",
   }
 ]
 
