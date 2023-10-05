df8313c7d0ca9820c1649c073c9b24e3ffe94e99
miguelbertoldi <miguelbertoldi1@gmail.com>
2023-10-05 19:20:15 -0300

    menu superior ok

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class TasksComponent {
 
   clicked: string = 'kanban';
+  searchBarOpen: boolean = false;
 
   menuItems = [
     { id: 'kanban', iconClass: 'pi pi-th-large', label: 'Kanban' },
@@ -15,6 +16,23 @@ export class TasksComponent {
     { id: 'calendar', iconClass: 'pi pi-calendar', label: 'Calendário' },
     { id: 'mural', iconClass: 'pi pi-chart-bar', label: 'Mural' }
 ];
+
+  configItems = [
+    { id: 'filter', iconClass: 'pi pi-filter', click: this.clickFilter() },
+    { id: 'order', iconClass: 'pi pi-arrow-right-arrow-left', click: this.clickOrder() },
+];
+
+  clickSearchBar(): void {
+    this.searchBarOpen = !this.searchBarOpen;
+  } 
+
+  clickFilter(): void {
+
+  }
+
+  clickOrder(): void {
+
+  }
 
   changePreviewMode(preview: string): void {
     this.clicked = preview;
