d87be4dcbdb86fea6ff08bfe6d76f08ea4c56d3f
anaborchardt <anaclaratomaselliborchardt75@gmail.com>
2023-10-26 19:23:17 -0300

    teste

--- src/app/pages/user-settings/notifications/notifications.component.ts
+++ src/app/pages/user-settings/notifications/notifications.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-notifications',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class NotificationsComponent {
 
+  faBell = faBell;
+  
 }
--- src/app/pages/user-settings/profile/profile.component.ts
+++ src/app/pages/user-settings/profile/profile.component.ts
@@ -37,7 +37,7 @@ export class ProfileComponent {
   buttonEdit: boolean = true;
   buttonConfirm: boolean = false;
   contentEditable: boolean = false;
-  click: string = 'edit';
+  click: string = 'initial';
   clickName: string = 'changeName';
 
   // Alter the status of toogle
