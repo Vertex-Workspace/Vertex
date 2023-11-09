df5da76eca79838a47b20fb43de8339c09f82634
anaborchardt <anaclaratomaselliborchardt75@gmail.com>
2023-11-08 21:56:39 -0300

    adiciona ngStyle, gradiente e funcionalidades da aparência

--- src/app/pages/user-settings/appearance/appearance.component.ts
+++ src/app/pages/user-settings/appearance/appearance.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faPencil, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faPencil, faSun, faMoon, faToggleOff, faToggleOn, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-appearance',
@@ -10,17 +10,66 @@ export class AppearanceComponent {
   faPencil = faPencil;
   faSun = faSun;
   faMoon = faMoon;
+  faToggleOn = faToggleOn;
+  faToggleOff = faToggleOff;
+  faCheck = faCheck;
 
   themesList = [
     {
-      mode: 'light', 
+      mode: 'Tema Claro', 
       icon: faSun,
-      colors: {
-        
-      }
     },
-    {mode: 'dark', icon: faMoon}
+    {
+      mode: 'Tema Escuro', 
+      icon: faMoon
+    }
   ]
+
+  pcolors = [
+      {first: 'pink', status: 'selected'},
+      {first: 'purple', status: 'unselected'},
+      {first: 'blue' , status: 'unselected'},
+      {first: 'green', status: 'unselected'},
+      {first: 'white', status: 'unselected'},
+      {first: 'black', status: 'unselected'},
+  ]
+
+  scolors = [
+    {first: 'pink'},
+    {first: 'purple'},
+    {first: 'blue'},
+    {first: 'green'},
+    {first: 'white'},
+    {first: 'black'},
+  ];
+
+  toggles = [
+    {text: "Habilitar comando de voz", icon: faToggleOff},
+    {text: "Habilitar leitura de texto", icon: faToggleOff},
+  ];
+
+  toggleChange(item: number): void{
+    if(this.toggles[item].icon == faToggleOn){
+      this.toggles[item].icon = faToggleOff;
+    }
+    else{
+      this.toggles[item].icon = faToggleOn;
+    }
+}
+
+
+fontSizes = [
+  {size: '8', status: 'unselected'},
+  {size: '10', status: 'selected'},
+  {size: '12', status: 'unselected'},
+]
+
+fontFamily = [
+  {font: "sla"},
+  {font: "sla"},
+  {font: "sla"}
+];
+
 
 
 }
