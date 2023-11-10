3830b75ac895437919450875c61d69a4e80f2744
anaborchardt <anaclaratomaselliborchardt75@gmail.com>
2023-11-09 21:56:26 -0300

    build.refactoring code of appearance

--- src/app/pages/user-settings/appearance/appearance.component.ts
+++ src/app/pages/user-settings/appearance/appearance.component.ts
@@ -14,62 +14,103 @@ export class AppearanceComponent {
   faToggleOff = faToggleOff;
   faCheck = faCheck;
 
-  themesList = [
+  themesList: any[] = [
     {
-      mode: 'Tema Claro', 
+      mode: 'Tema Claro',
       icon: faSun,
+      status: 'selected',
+      types: [
+        {
+          title: "Cor de Fundo",
+          colors: [
+            { 'color': '#FFFFFF', status: 'unselected' },
+            { 'color': '#F3F3F3', status: 'unselected' },
+            { 'color': '#E5DDF5', status: 'unselected' },
+            { 'color': '#DCE8F2', status: 'unselected' },
+            { 'color': '#FAEAEA', status: 'unselected' },
+            { 'color': '#F5FCF2', status: 'unselected' },
+          ]
+        },
+        {
+          title: "Cor Secundária",
+          colors: [
+            { 'color': '#092C4C', status: 'unselected' },
+            { 'color': '#F5B1B1', status: 'unselected' },
+            { 'color': '#6F57A1', status: 'unselected' },
+            { 'color': '#000000', status: 'unselected' },
+            { 'color': '#A5A973', status: 'unselected' },
+            { 'color': '#83B172', status: 'unselected' },
+          ]
+        }
+      ]
     },
     {
-      mode: 'Tema Escuro', 
-      icon: faMoon
+      mode: 'Tema Escuro',
+      icon: faMoon,
+      status: 'unselected',
+      types: [
+        {
+          title: "Cor de Fundo",
+          colors: [
+            { 'color': '#574444', status: 'unselected' },
+            { 'color': '#5B5555', status: 'unselected' },
+            { 'color': '#0B2740', status: 'unselected' },
+            { 'color': '#464646', status: 'unselected' },
+            { 'color': '#787878', status: 'unselected' },
+            { 'color': '#322438', status: 'unselected' },
+          ]
+        },
+        {
+          title: "Cor Secundária",
+          colors: [
+            { 'color': '#F3F3F3', status: 'unselected' },
+            { 'color': '#F5B1B1', status: 'unselected' },
+            { 'color': '#3D05B2', status: 'unselected' },
+            { 'color': '#BEDFF4', status: 'unselected' },
+            { 'color': '#FFC1C9', status: 'unselected' },
+            { 'color': '#83B172', status: 'unselected' },
+          ]
+        }
+      ]
     }
   ]
-
-  pcolors = [
-      {first: 'pink', status: 'selected'},
-      {first: 'purple', status: 'unselected'},
-      {first: 'blue' , status: 'unselected'},
-      {first: 'green', status: 'unselected'},
-      {first: 'white', status: 'unselected'},
-      {first: 'black', status: 'unselected'},
-  ]
-
-  scolors = [
-    {first: 'pink'},
-    {first: 'purple'},
-    {first: 'blue'},
-    {first: 'green'},
-    {first: 'white'},
-    {first: 'black'},
-  ];
 
   toggles = [
-    {text: "Habilitar comando de voz", icon: faToggleOff},
-    {text: "Habilitar leitura de texto", icon: faToggleOff},
+    { text: "Habilitar comando de voz", icon: faToggleOff },
+    { text: "Habilitar leitura de texto", icon: faToggleOff },
   ];
 
-  toggleChange(item: number): void{
-    if(this.toggles[item].icon == faToggleOn){
+  toggleChange(item: number): void {
+    if (this.toggles[item].icon == faToggleOn) {
       this.toggles[item].icon = faToggleOff;
     }
-    else{
+    else {
       this.toggles[item].icon = faToggleOn;
     }
-}
+  }
 
 
-fontSizes = [
-  {size: '8', status: 'unselected'},
-  {size: '10', status: 'selected'},
-  {size: '12', status: 'unselected'},
-]
+  fontSizes = [
+    '12 (Padrão)', '14'
+  ]
 
-fontFamily = [
-  {font: "sla"},
-  {font: "sla"},
-  {font: "sla"}
-];
+  fontFamily = [
+    'Arial', 'Helvetica', 'Times New Roman'
+  ];
 
+  selectColor(type:any,item: number): void {
 
+    type.colors.forEach((element: { status: string; }) => {
+      element.status = 'unselected';
+    });
+    type.colors[item].status = 'selected';
+  }
 
+  selectTheme(item : number): void {
+    this.themesList.forEach((element: { status: string; }) => {
+      element.status = 'unselected';
+    });
+    this.themesList[item].status = 'selected';
+    console.log(this.themesList[item].status)
+  }
 }
