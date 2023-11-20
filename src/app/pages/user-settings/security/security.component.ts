7f1381eec2f8500f23227c1a0932abb062734d3a
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-11-20 14:33:26 -0300

    edit default fonts to user settings

--- src/app/pages/user-settings/security/security.component.ts
+++ src/app/pages/user-settings/security/security.component.ts
@@ -7,50 +7,44 @@ import { faLock, faEye, faKey } from '@fortawesome/free-solid-svg-icons'
   styleUrls: ['./security.component.scss']
 })
 export class SecurityComponent {
-    faLock = faLock;
-    faKey = faKey;
-    contentEditable: boolean = false;
-    click: string = 'initial';
-    statusEye: boolean = false;
-    validPassword: boolean = true;
+  faLock = faLock;
+  faKey = faKey;
+  contentEditable: boolean = false;
+  click: string = 'initial';
+  validPassword: boolean = true;
 
-    passwords = [
-        {value: '', icon: faKey, label:'Confirme sua senha antiga', status:false},
-        {value: '', icon: faLock, label:'Digite uma nova senha', status:false},
-        {value: '', icon: faLock, label:'Confirme sua nova senha', status:false},
-    ]
+  passwords = [
+    { value: '', icon: faKey, label: 'Confirme sua senha antiga', status: false, statusEye: false },
+    { value: '', icon: faLock, label: 'Digite uma nova senha', status: false, statusEye: false  },
+    { value: '', icon: faLock, label: 'Confirme sua nova senha', status: false, statusEye: false  },
+  ]
 
-    clickOption(id: string): void{
-      this.click = id;
-      this.contentEditable = !this.contentEditable
-      console.log(this.click)
-    }
-  
-      onChange(item : number): void {
-      this.passwords[item].status = true;
-      console.log(this.passwords[item].status)
-      if((this.passwords[0].status == true) &&
+  clickOption(id: string): void {
+    this.click = id;
+    this.contentEditable = !this.contentEditable
+  }
+
+  onChange(item: number): void {
+    this.passwords[item].status = true;
+    if ((this.passwords[0].status == true) &&
       (this.passwords[1].status == true) &&
-      (this.passwords[2].status == true)){
+      (this.passwords[2].status == true)) {
       this.contentEditable = true;
-      }
-      console.log(this.contentEditable)
     }
+  }
 
-    passwordEye(): void {
-      this.statusEye = !this.statusEye;
-      console.log(1)
-    }
+  passwordEye(item: any): void {
+    item.statusEye = !item.statusEye;
+  }
 
-    
+
   passwordValidation(): void {
-    console.log(1);
-    if(this.passwords[1].value != this.passwords[2].value){
+    if (this.passwords[1].value != this.passwords[2].value) {
       this.validPassword = false;
     }
   }
 
-  closeModal(){
+  closeModal() {
     this.validPassword = true;
   }
 
