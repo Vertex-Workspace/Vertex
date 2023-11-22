562cc12bf2d08359bfdc6548f25c2f061c188b9b
miguelbertoldi <miguelbertoldi1@gmail.com>
2023-11-22 19:59:07 -0300

    corrigido

--- src/app/pages/tasks/tasks.module.ts
+++ src/app/pages/tasks/tasks.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { TasksComponent } from './tasks.component';
 import { ListComponent } from './list/list.component';
 import { KanbanComponent } from './kanban/kanban.component';
-import { CalendarComponent } from './calendar/calendar.component';
 import { TaskModule } from 'src/app/components/modals/task/task.module';
 import { TaskComponent } from 'src/app/components/modals/task/task.component';
 import { RouterModule } from '@angular/router';
@@ -22,9 +21,7 @@ import { MuralComponent } from './mural/mural.component';
 @NgModule({
   declarations: [
     TasksComponent,
-    KanbanComponent,
     MuralComponent,
-    CardComponent,
     MuralCardComponent,
    ],
   exports:[
@@ -38,7 +35,7 @@ import { MuralComponent } from './mural/mural.component';
     DragDropModule,
     FormsModule,
     CalendarModule,
-    ListModule
+    ListModule,
     KanbanModule
   ],
 })
